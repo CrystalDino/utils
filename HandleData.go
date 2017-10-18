2fe9f33ce0e8bb1a89d2ba5d7e14fb733c212484
dino <crystal.dino@hotmail.com>
2017-10-18 18:46:21 +0800

    add methods for trasform with uint32 and ip

--- HandleData.go
+++ HandleData.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"bytes"
 	"errors"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -51,4 +53,40 @@ func StructToMap(data interface{}, lowerKey, bExcept bool, fields ...string) (m 
 		}
 	}
 	return
+}
+
+func IpToUint32(ipStr string) uint32 {
+	parts := strings.Split(ipStr, ".")
+	if len(parts) != 4 {
+		return 0
+	}
+	var (
+		dst uint64
+	)
+	for k, v := range parts {
+		tmp, err := strconv.ParseUint(v, 10, 32)
+		if err != nil {
+			return 0
+		}
+		dst |= tmp << uint((3-k)*8)
+	}
+	return uint32(dst)
+}
+
+func Uint32ToIp(ipUint uint32) string {
+	ipSegs := make([]string, 4)
+	length := len(ipSegs)
+	buffer := bytes.NewBufferString("")
+	for i := 0; i < length; i++ {
+		tempUint := ipUint & 0xFF
+		ipSegs[length-i-1] = strconv.FormatUint(uint64(tempUint), 10)
+		ipUint = ipUint >> 8
+	}
+	for i := 0; i < length; i++ {
+		buffer.WriteString(ipSegs[i])
+		if i < length-1 {
+			buffer.WriteString(".")
+		}
+	}
+	return buffer.String()
 }
